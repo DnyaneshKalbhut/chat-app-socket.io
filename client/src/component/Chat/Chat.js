59fa840d315fa5f00ac52823ca86046459159f43
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2024-03-13 01:00:31 +0530

    backend deployed

--- client/src/component/Chat/Chat.js
+++ client/src/component/Chat/Chat.js
@@ -7,7 +7,7 @@ import Message from "../Message/Message";
 import ReactScrollToBottom from "react-scroll-to-bottom";
 import closeIcon from '../../Image/closeIcon.png'
 
-const ENDPOINT = "http://localhost:4500/";
+const ENDPOINT = "https://chat-app-socket-io-ekz9.onrender.com";
 let socket;
 // ... (imports)
 
